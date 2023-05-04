7115b63a5d4208156c42189d5c8bd11afc02ce70
Tanvir hossain <tanvirhossain4393@gamil.com>
2023-05-04 21:46:27 +0600

    active route added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../Providers/AuthProviders';
 
 const Header = () => {
     const { user, logOut } = useContext(AuthContext)
-   
+    const activeLink = "text-blue-500 font-bold mr-3 ml-3";
+
 
     const handleLogOut = () => {
         logOut()
-        .then()
-        .catch(error =>{
-            console.log(error)
-        })
+            .then()
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     return (
@@ -23,26 +24,26 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <Link to="/">Home</Link>
-                            <Link to="/blog">Blog</Link>
+                            <NavLink to="/" className={({ isActive }) => (isActive ? activeLink : "")}>Home</NavLink>
+                            <NavLink to="/blog" className={({ isActive }) => (isActive ? activeLink : "")}>Blog</NavLink>
                         </ul>
                     </div>
                     <a className="btn btn-ghost normal-case text-xl">Food Village</a>
                 </div>
                 <div className="navbar-center hidden lg:flex text-xl">
 
-                    <Link className='' to="/">Home</Link>
-                    <Link className='ms-5' to="/blog">Blog</Link>
+                    <NavLink to="/" className={({ isActive }) => (isActive ? activeLink : "")}>Home</NavLink>
+                    <NavLink to="/blog" className={({ isActive }) => (isActive ? activeLink : "")}>Blog</NavLink>
 
 
 
                 </div>
                 <div className="navbar-end">
-                   
+
                     {
                         user ? <div className="avatar mr-4">
                             <div className="w-10 rounded-full mr-4">
-                                {user.photoURL? <img src={user.photoURL} alt='Photo' title={user.displayName} />:<img src='' alt='Photo' title={user.displayName} />}
+                                {user.photoURL ? <img src={user.photoURL} alt='Photo' title={user.displayName} /> : <img src='' alt='Photo' title={user.displayName} />}
                             </div>
                             <Link onClick={handleLogOut} className="btn">Logout</Link>
                         </div> : <Link to="/login" className="btn">Login</Link>
